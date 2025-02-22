d33a74ff4547c3555c16afb45e69bd225ea7e14d
Kleber Goncalves <kleber.goncalves.1209@gmail.com>
2025-02-22 19:39:17 -0300

    Atua

--- dist/main.js
+++ dist/main.js
@@ -4,16 +4,21 @@ const core_1 = require("@nestjs/core");
 const swagger_1 = require("@nestjs/swagger");
 const app_module_1 = require("./app.module");
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    const config = new swagger_1.DocumentBuilder()
-        .setTitle("API documentação - Contabil")
-        .setDescription("API description for all tables")
-        .setVersion("1.0")
-        .build();
-    const document = swagger_1.SwaggerModule.createDocument(app, config);
-    swagger_1.SwaggerModule.setup("api", app, document);
-    await app.listen(3000);
-    console.log(`Application is running on: ${await app.getUrl()}`);
+    try {
+        const app = await core_1.NestFactory.create(app_module_1.AppModule);
+        const config = new swagger_1.DocumentBuilder()
+            .setTitle("API ERP Seuros - Nuxon")
+            .setDescription("Descrição da API para todas as tabelas")
+            .setVersion("1.0")
+            .build();
+        const document = swagger_1.SwaggerModule.createDocument(app, config);
+        swagger_1.SwaggerModule.setup("api", app, document);
+        await app.listen(process.env.PORT || 3000);
+        console.log(`Application is running on: ${await app.getUrl()}`);
+    }
+    catch (error) {
+        console.error("Application failed to start:", error);
+    }
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- src/main.ts
+++ src/main.ts
@@ -3,18 +3,22 @@ import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger"
 import { AppModule } from "./app.module"
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
+  try {
+    const app = await NestFactory.create(AppModule)
 
-  const config = new DocumentBuilder()
-    .setTitle("API documentação - Contabil")
-    .setDescription("API description for all tables")
-    .setVersion("1.0")
-    .build()
-  const document = SwaggerModule.createDocument(app, config)
-  SwaggerModule.setup("api", app, document)
+    const config = new DocumentBuilder()
+      .setTitle("API ERP Seguros - Nuxon")
+      .setDescription("Descrição da API para todas as tabelas")
+      .setVersion("1.0")
+      .build()
+    const document = SwaggerModule.createDocument(app, config)
+    SwaggerModule.setup("api", app, document)
 
-  await app.listen(3000)
-  console.log(`Application is running on: ${await app.getUrl()}`)
+    await app.listen(process.env.PORT || 3000)
+    console.log(`Application is running on: ${await app.getUrl()}`)
+  } catch (error) {
+    console.error("Application failed to start:", error)
+  }
 }
 bootstrap()
 
